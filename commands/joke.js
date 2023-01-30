232178e32d13e7925ac592857eee22bf8060b470
Scott Maitland <scott@maitland.me.uk>
2023-01-30 20:32:27 +0000

    Committing joke command to bot

--- commands/joke.js
+++ commands/joke.js
@@ -1,28 +1,6 @@
 const { SlashCommandBuilder } = require("discord.js");
 const { execute } = require("./ping");
 
-// const jokeResponse = async () =>
-//   fetch("https://icanhazdadjoke.com/", {
-//     method: "GET",
-//     headers: {
-//       Accept: "application/json"
-//     }
-//   })
-//     .then(function (response) {
-//       if (response.ok) {
-//         return response.json();
-//       }
-
-//       return Promise.reject(response);
-//     })
-//     .then(function (data) {
-//       console.log(data.joke);
-//       return data.joke;
-//     })
-//     .catch(function (err) {
-//       console.warn("Something went wrong.", err);
-//     });
-
 async function tellJoke() {
   let response = await fetch("https://icanhazdadjoke.com/", {
     method: "GET",
@@ -35,14 +13,13 @@ async function tellJoke() {
   return await data.joke;
 }
 
-const jokeResponse = tellJoke();
-
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("joke")
     .setDescription("Tell me a random joke"),
   async execute(interaction) {
-    await interaction.deferReply();
+    await interaction.deferReply({ ephemeral: true });
+    const jokeResponse = await tellJoke();
     await interaction.followUp({
       content: `${jokeResponse}`,
       ephemeral: true
